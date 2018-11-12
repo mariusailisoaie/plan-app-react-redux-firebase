ae464cf303c4a8727e8506bd1b7b3746ddfde46b
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-12 22:56:01 +0100

    Update: navbar color, project details id added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper blue lighten-3">
+    <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className="brand-logo">ProjectManager</Link>
         <SignedInLinks />
--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project title</span>
+          <span className="card-title">Project title - {id}</span>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore rerum, alias laboriosam temporibus, quibusdam fuga architecto quae repudiandae ex odit inventore. Maxime non alias doloremque dignissimos maiores, fuga provident ullam?</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
