41082358f5c6f6a669632404930779418060bedb
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-12 22:37:41 +0100

    Add: components, routing

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project title</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore rerum, alias laboriosam temporibus, quibusdam fuga architecto quae repudiandae ex odit inventore. Maxime non alias doloremque dignissimos maiores, fuga provident ullam?</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Marius Ailisoaie</div>
+          <div>12 November 2018</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails;
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project title</span>
+        <p>Posted by Marius Ailisoaie</p>
+        <p className="grey-text">12 November 2018</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
