bfb3e63c1ee2ebc5f0ac8c80854461410424cedf
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-13 23:36:52 +0100

    Add: redux store and pass data as props in components

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by Marius Ailisoaie</p>
         <p className="grey-text">12 November 2018</p>
       </div>
--- src/components/store/reducers/projectReducer.js
+++ src/components/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+  projects: [
+    {id: "1", title: "Finding Mario"},
+    {id: "2", title: "The universe is big"},
+    {id: "3", title: "The car of the year"}
+  ]
 }
 
 const projectReducer = (state = initState, action) => {
