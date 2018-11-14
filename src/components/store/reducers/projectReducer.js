3d6969fbe1bf1c1b7da01e4c8b08747f5ac47492
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-14 10:26:29 +0100

    Add: redux thunk

--- src/components/store/reducers/projectReducer.js
+++ src/components/store/reducers/projectReducer.js
@@ -1,12 +1,21 @@
 const initState = {
   projects: [
-    {id: "1", title: "Finding Mario"},
-    {id: "2", title: "The universe is big"},
-    {id: "3", title: "The car of the year"}
+    {id: "1", title: "Finding Mario", content: 'ad'},
+    {id: "2", title: "The universe is big", content: 'ad'},
+    {id: "3", title: "The car of the year", content: 'ad'}
   ]
 }
 
 const projectReducer = (state = initState, action) => {
+  switch(action.type) {
+    case 'CREATE_PROJECT':
+      console.log('created project', action.project);
+      break;
+    default: 
+      console.log('default');
+      break;
+  }
+  
   return state;
 }
 
