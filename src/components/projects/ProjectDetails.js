397de998053e44c9368740c473f25d63c14945d8
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-16 22:37:47 +0100

    Add: get user data from user profile from db, moment.js for date

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { firestoreConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const ProjectDetails = (props) => {
   const { project, authStatus } = props;
@@ -18,7 +19,7 @@ const ProjectDetails = (props) => {
           </div>
           <div className="card-action grey lighten-4 grey-text">
             <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
-            <div>12 November 2018</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
